f29f6fb50ee03d6f95e03eeb82e8953f12cb4337
GhilbranRafif <mulyaghilbran@gmail.com>
2024-10-29 15:23:02 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Ghilbran Rafif Mulya"; // TODO: replace this
+const studentId = "FS11109397"; // TODO: replace this
+
+export { url, studentName, studentId };
